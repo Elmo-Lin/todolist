317bc068ead1921ddd54775ab91b0df512bb4e47
Elmo Lin <elmo.lin@hpe.cpm>
2024-08-20 07:44:07 +0800

    3-2

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,19 @@
 import React, { Component, Fragment } from "react";
 
 class TodoList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+      list: []
+    }
+  }
+  
   render() {
     return (
       <Fragment>
-        <div><input /><button>submit</button></div>
+        <div><input value={this.state.inputValue} onChange={this.handleInputChange.bind(this)} /><button>submit</button></div>
         <ul>
           <li>learn react</li>
           <li>learn redux</li>
@@ -12,6 +21,12 @@ class TodoList extends Component {
         </ul>
       </Fragment>
     );
+  }
+
+  handleInputChange(e) {
+    this.setState({
+      inputValue: e.target.value
+    })
   }
 }
 
